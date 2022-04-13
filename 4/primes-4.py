d836f1a18fa3093f2944b70d7583f6d331e9b6c6
mbakhterev <mob@k.imm.uran.ru>
2022-04-14 00:50:23 +0500

    FIX: Проблемы с индексами в версии 4. Исправлен код на Python

--- 4/primes-4.py
+++ 4/primes-4.py
@@ -12,55 +12,48 @@ def isqrt(x):
             z, r = t, r+q
     return r
 
-def init_markvector(limit):
-    assert(limit > 1)
-    M = bytearray([1] * (limit + 1))
+def init_marks(N):
+    assert(N > 1)
+    M = bytearray([1] * N)
     M[0] = 0
     M[1] = 1
     return M
 
-def vector_limit(A, L): return min(L, len(A) - 1)
+def marks_limit(A, L): return min(L, len(A))
 
-def sieve(M, limit, p, cursor):
-    l = vector_limit(M, limit)
-    c = cursor
-    while c <= l:
+def sieve(M, N, p, c):
+    L = marks_limit(M, N)
+    while c < L:
         M[c] = 0
         c += p
-    return c - l - 1
+    return c - L
 
-def next_prime_offset(M, limit, start):
-    l = vector_limit(M, limit)
+def next_prime_offset(M, N, start):
+    L = marks_limit(M, N)
     i = start + 1 + (start & 1)
-    while i <= l and M[i] == 0: i += 2
-    if i <= l: return i
-    return limit + 1
+    while i < L and M[i] == 0: i += 2
+    return i
 
-def optimus_primes():
-    N = int(sys.argv[1])
-    L = isqrt(N)
+def marks_sum(M, N):
+    s = 0
+    for i in range(0, marks_limit(M, N)):
+        s += M[i]
+    return s
+
+def optimus_primes(N):
     P = []
     C = []
-    if L < 2:
-        M = bytearray([0] * L)
-    else:
-        M = init_markvector(L)
+    M = init_marks(N) if N >= 2 else bytearray([0] * L)
     p = 2
-    while p <= L:
+    while p < L:
         P.append(p)
-        C.append(sieve(M, L, p, p + p))
-        p = next_prime_offset(M, L, p)
-    return N, M, P, C
+        C.append(sieve(M, N, p, p + p))
+        p = next_prime_offset(M, N, p)
+    return M, P, C
 
 def reset(M):
     for i in range(0, len(M)):
         M[i] = 1
-
-def limited_sum(A, L):
-    s = 0
-    for i in range(0, vector_limit(A, L) + 1):
-        s += A[i]
-    return s
 
 def sieve_recursor_count(N, M, P, C):
     reset(M)
@@ -68,16 +61,17 @@ def sieve_recursor_count(N, M, P, C):
         p = P[i]
         c = C[i]
         C[i] = sieve(M, N, p, c)
-    return limited_sum(M, N)
+    return marks_sum(M, N)
 
-N, M, P, C = optimus_primes()
+N = int(sys.argv[1])
+L = isqrt(N) + 1
+M, P, C = optimus_primes(L)
 if len(P) == 0:
     print(0)
-
-L = len(M)
-n = len(P)
-left = N - L
-while left > 0:
-    n += sieve_recursor_count(left, M, P, C)
-    left -= L
-print(n)
+else:
+    n = len(P)
+    l = (N + 1) - L
+    while l > 0:
+        n += sieve_recursor_count(l, M, P, C)
+        l -= L
+    print(n)
