cce64c6e8db238e8f9adc4cea20ddf9ff1624741
mbakhterev <mob@k.imm.uran.ru>
2022-04-14 00:50:23 +0500

    FIX: Проблемы с индексами в версии 4. Исправлен код на Haskell

--- 4/primes-4-64.hs
+++ 4/primes-4-64.hs
@@ -25,37 +25,44 @@ type Primes s = STUArray s Int64 Int64
 type Cursors s = STUArray s Int64 Int64
 
 initMarks :: Int64 -> ST s (Marks s)
-initMarks limit =
-  if limit < 2
-  then error "Limit is too low"
-  else do a <- newArray (0, limit) 1
+initMarks nN =
+  if nN > 1
+  then do a <- newArray (0, nN - 1) 1
           writeArray a 0 0
           writeArray a 1 0
           return a
+  else error "Limit is too low"
+
 
 marksCapacity :: Marks s -> ST s Int64
 marksCapacity aM = getNumElements aM >>= return . fromIntegral
 
 primesCapacity :: Primes s -> ST s Int64
-primesCapacity aM = getNumElements aM >>= return . fromIntegral
+primesCapacity aP = getNumElements aP >>= return . fromIntegral
 
 marksLimit :: Marks s -> Int64 -> ST s Int64
-marksLimit aM l = marksCapacity aM >>= \capacity -> return (min l capacity)
+marksLimit aM l = marksCapacity aM >>= return . min l
 
 sieve :: Marks s -> Int64 -> Int64 -> Int64 -> ST s Int64
-sieve aM limit p cursor = marksLimit aM limit >>= go cursor
-  where go c l = if c <= l
+sieve aM nN p cursor = marksLimit aM nN >>= go cursor
+  where go c l = if c < l
                  then writeArray aM c 0 >> go (c + p) l
-                 else return (c - l - 1)
+                 else return (c - l)
 
 nextPrimeOffset :: Marks s -> Int64 -> Int64 -> ST s Int64
-nextPrimeOffset aM limit start = marksLimit aM limit >>= go (start + 1 + (start .&. 1))
-  where go i l = if i > l
-                 then return (l + 1)
-                 else do v <- readArray aM i
+nextPrimeOffset aM nN start = marksLimit aM nN >>= go (start + 1 + (start .&. 1))
+  where go i l = if i < l
+                 then do v <- readArray aM i
                          if v == 0
                          then go (i + 2) l
-                         else return i;
+                         else return i
+                 else return i
+
+sumMarks :: Marks s -> Int64 -> ST s Int64
+sumMarks aM nN = marksLimit aM nN >>= \l -> go l 0 0
+  where go l i s = if i < l
+                   then readArray aM i >>= go l (i + 1) . (+ s) . fromIntegral
+                   else return s
 
 compactify :: [Int64] -> [Int64] -> Int64 -> ST s (Primes s, Cursors s)
 compactify primes cursors n = do pV <- newArray (0, n - 1) 0
@@ -67,20 +74,13 @@ compactify primes cursors n = do pV <- newArray (0, n - 1) 0
                                       go pV cV (i - 1) ps cs
 
 optimusPrimes :: Int64 -> ST s (Marks s, Primes s, Cursors s)
-optimusPrimes nN = (if nL < 2 then newArray (0, nL) 0 else initMarks nL) >>= go 2 0 [] []
-  where nL = isqrt nN
-        go p n ps cs aM = if p <= nL
-                          then do np <- nextPrimeOffset aM nL p
-                                  c <- sieve aM nL p (p + p)
+optimusPrimes nN = (if nN < 2 then newArray (0, nN) 0 else initMarks nN) >>= go 2 0 [] []
+  where go p n ps cs aM = if p < nN
+                          then do np <- nextPrimeOffset aM nN p
+                                  c <- sieve aM nN p (p + p)
                                   go np (n + 1) (p:ps) (c:cs) aM
                           else do (primes, cursors) <- compactify ps cs n
                                   return (aM, primes, cursors)
-
-sumMarks :: Marks s -> Int64 -> ST s Int64
-sumMarks aM nL = marksLimit aM nL >>= \l -> go l 0 0
-  where go l i s = if i <= l
-                   then readArray aM i >>= go l (i + 1) . (+ s) . fromIntegral
-                   else return s
 
 marksReset :: Marks s -> ST s ()
 marksReset aM = getBounds aM >>= mapM_ (\i -> writeArray aM i 1) . range
@@ -95,13 +95,15 @@ sieveRecursorCount nN aM aP aC = marksReset aM >> primesCapacity aP >>= go 0
                  else sumMarks aM nN;
 
 process :: Int64 -> ST s Int64
-process nN = do (aM, aP, aC) <- optimusPrimes nN
-                pl <- primesCapacity aP
-                if pl == 0
-                then return 0
-                else let go nL left n = if left > 0
-                                        then sieveRecursorCount left aM aP aC >>= go nL (left - nL) . (+ n)
-                                        else return n
-                      in marksCapacity aM >>= \nL -> go nL (nN - nL) pl
+process nN =
+  let nL = (isqrt nN) + 1
+  in do (aM, aP, aC) <- optimusPrimes nL
+        pl <- primesCapacity aP
+        if pl == 0
+        then return 0
+        else let go nL l n = if l > 0
+                             then sieveRecursorCount l aM aP aC >>= go nL (l - nL) . (+ n)
+                             else return n
+             in marksCapacity aM >>= \nL -> go nL ((nN + 1) - nL) pl
 
 main = getArgs >>= (return . read . head) >>= \nN -> print (runST (process nN))
