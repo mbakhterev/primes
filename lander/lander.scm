e3231890819a853ab4669ad5eeed720263cd4686
mbakhterev <mob@k.imm.uran.ru>
2022-04-18 21:19:51 +0500

    WIP: Lander

--- lander/lander.scm
+++ lander/lander.scm
@@ -1,62 +1,57 @@
+(define-syntax get
+  (let ((accessors (lambda (r fs)
+                     (let ((p (symbol->string (syntax->datum r))))
+                       (map (lambda (f)
+                              (string->symbol
+                                (string-append p "-" (symbol->string f))))
+                            (syntax->datum fs))))))
+    (lambda (x)
+      (syntax-case x ()
+        ((k v r fs ...)
+         (with-syntax (((as ...) (datum->syntax (syntax k)
+                                                (accessor (syntax r)
+                                                          (syntax (fs ...))))))
+           (syntax (values (as v) ...))))))))
+
 (define x-max (fl- 7000.0 1.0))
 (define y-max (fl- 3000.0 1.0))
 
 (define-record point ((immutable double x) (immutable double y)))
-(define-values (:x :y) (values point-x point-y))
 
 (define-record section ((immutable double ax) (immutable double ay)
                         (immutable double bx) (immutable double by)
                         (immutable double nx) (immutable double ny)))
 
-; (define-values (:ax :ay :bx :by :nx :ny) (values section-ax section-ay
-;                                                  section-bx section-by
-;                                                  section-nx section-ny))
- 
-(define-record landscape ((immutable landing-pad)
-                          (immutable left-rock)
-                          (immutable right-rock)
-                          (immutable l-rock)
-                          (immutable r-rock)
-                          (immutable raw-surface)))
-; (define-values (:landing-pad :left-rock :right-rock :l-rock :r-rock :raw-surface)
-;   (values landscape-landing-pad
-;           landscape-left-rock
-;           landscape-right-rock
-;           landscape-l-rock
-;           landsacpe-r-rock
-;           landscape-raw-surface))
-
 (define-record stage ((immutable double x-target) (immutable double x-opposite)
                       (immutable double x-pad) (immutable double y-pad)
                       (immutable double direction)
                       section stage surface))
-; (define-values (:x-target :x-opposite
-;                 :x-pad :y-pad
-;                 :direction
-;                 :section :stage :surface)
-;   (values stage-x-target stage-x-opposite
-;           stage-x-pad stage-y-pad
-;           stage-direction
-;           stage-section stage-stage stage-surface))
 
 (define-values (non-zero? near-zero?)
   (let ((ε 1e-10))
     (values (lambda (x) (fl< ε (flabs x)))
             (lambda (x) (fl<= (flabs x) ε)))))
 
+(define (normal a b)
+  (let-values (((ax ay) (get a point x y))
+               ((bx by) (get b point x y)))
+    (let* ((dx (fl- bx ax))
+           (dy (fl- by ay))
+           (len (flsqrt (fl+ (fl* dx dx) (fl* dy dy)))))
+      (assert (non-zero? len))
+      (values (fl- (fl/ dy len)) (fl/ dx len)))))
+
 (define (form-section a b)
-  (define dxdy (lambda (a b) (values (fl- (point-x a) (point-x b)) (fl- (point-y a) (point-y b)))))
-  (define (normal a b)
-    (let-values (((dx dy) (dxdy b a)))
-      (let ((len (flsqrt (fl+ (fl* dx dx) (fl* dy dy)))))
-        (assert (non-zero? len))
-        (values (fl- (fl/ dy len)) (fl- (fl/ dx len))))))
-  (let-values (((nx ny) (normal a b)))
-    (make-section (:x a) (:y a) (:x b) (:y b) nx ny)))
+  (let-values (((ax ay) (get a point x y))
+               ((bx by) (get b point x y))
+               ((nx ny) (normal a b))) 
+    (make-section ax ay bx by nx ny)))
 
 (define (normal-projection s tx ty)
-  (let ((x (:ax s))
-        (y (:ay s))
-        ()
-        ) (fl+ (fl* (:nx s) (fl- tx (:x s)))))
-  )
+  (let-values (((x y nx ny) (get s section ax ay nx ny)))
+    (fl+ (fl* nx (fl- tx x))
+         (fl* ny (fl- ty y)))))
+
+(define (over-line? s x y) (flpositive? (normal-projection s x y)))
+(define (in-range? x s)
+  (let-values (((ax bx) (get s section ax bx))) (and (fl<= ax x) (fl< x bx))))
--- primes/2/primes-2.c
+++ primes/2/primes-2.c
@@ -1,6 +1,6 @@
-#include <stdio.h>                                                                                                     
-#include <stdlib.h>                                                                                                    
-#include <unistd.h>                                                                                                    
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
                                                                                                                        
 typedef struct {                                                                                                       
   long *restrict buf;                                                                                                  
